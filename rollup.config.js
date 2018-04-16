1fd6f771f5843e05d54e5acaf02385cb0fe8d3a3
Dominik Bułaj <dominik@bulaj.com>
2018-04-16 11:02:25 +0200

    Switching to rollup.js as a build system

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  external: [
+    'react',
+    'prop-types',
+    'recompose',
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
+}
