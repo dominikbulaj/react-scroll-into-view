3814f03b05cb6ee511a9d1d262fa3431226ffa17
Dominik Bułaj <dominik@bulaj.com>
2021-05-12 11:47:03 +0200

    Added new scrollOptions to TS typings

--- react-scroll-into-view.d.ts
+++ react-scroll-into-view.d.ts
@@ -1,3 +1,8 @@
+interface ScrollOptions {
+    behavior?: 'auto' | 'smooth',
+    block?: 'start' | 'center' | 'end' | 'nearest',
+    inline?: 'start' | 'center' | 'end' | 'nearest',
+}
 interface ScrollIntoViewProps {
     selector: string,
     children: JSX.Element[] | JSX.Element,
@@ -5,8 +10,9 @@ interface ScrollIntoViewProps {
     style?: object,
     alignToTop?: boolean,
     className?: string,
+    scrollOptions?: ScrollOptions
 }
 
-const ScrollIntoView: React.FunctionComponent<ScrollIntoViewProps> = props => {}
+const ScrollIntoView: React.FunctionComponent<ScrollIntoViewProps> = props => { }
 
 export default ScrollIntoView
