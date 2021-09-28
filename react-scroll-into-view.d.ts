926086ce45490fa3ab56485a25759047d375db8d
Dominik Bułaj <dominik@bulaj.com>
2021-09-28 11:45:36 +0200

    Adjusted TypeScript declaration file

--- react-scroll-into-view.d.ts
+++ react-scroll-into-view.d.ts
@@ -3,16 +3,17 @@ interface ScrollOptions {
     block?: 'start' | 'center' | 'end' | 'nearest',
     inline?: 'start' | 'center' | 'end' | 'nearest',
 }
-interface ScrollIntoViewProps {
+export interface ScrollIntoViewProps {
     selector: string,
     children: JSX.Element[] | JSX.Element,
     smooth?: boolean,
     style?: object,
     alignToTop?: boolean,
     className?: string,
-    scrollOptions?: ScrollOptions
+    scrollOptions?: ScrollOptions,
+    onClick?: (ev: MouseEvent) => unknown,
 }
 
-const ScrollIntoView: React.FunctionComponent<ScrollIntoViewProps> = props => { }
+declare const ScrollIntoView: React.FunctionComponent<ScrollIntoViewProps>
 
 export default ScrollIntoView
