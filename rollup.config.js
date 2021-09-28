e6f858324ed3d226afd45f9ad3aee095aab294c9
Dominik Bułaj <dominik@bulaj.com>
2021-09-28 10:14:49 +0200

    Upgrading rollup to version 2 with it's plugins. Package output has now ESM module instead of CJS.

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { babel } from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
-    format: 'cjs'
+    format: 'es'
   },
   plugins: [
-    commonjs({
-      include: 'node_modules/**'
-    }),
-    resolve(),
+    nodeResolve(),
     babel({
       babelrc: false,
       presets: ['@babel/preset-env', '@babel/preset-react'],
