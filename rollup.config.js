6f9aceef3d7123493f521a3b8830ecf81f611d38
Dominik Bułaj <dominik@bulaj.com>
2022-01-09 16:22:56 +0100

    Generating both ESM & CJS modules

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,16 @@ import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'es'
-  },
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'es'
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs'
+    }
+  ],
   plugins: [
     nodeResolve(),
     babel({
